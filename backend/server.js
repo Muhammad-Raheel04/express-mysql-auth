e3184ac9b5551d5a5e1f8fec21d2be39b46ee9a5
Muhammad Raheel <raheel31102004@gmail.com>
2026-06-25 16:35:45 +0500

    feat: initialize Express server with global error handler

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
+import './config/db.js';
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(cors());
+
+app.use((err, req, res, next) => {
+    err.statusCode = err.statusCode || 500;
+    err.message = err.message || "Internal Server Error";
+
+    res.status(err.statusCode).json({
+        success: false,
+        message: err.message,
+    })
+})
+
+app.listen(PORT, () => {
+    console.log(`Visit http://localhost:${PORT}`)
+})
