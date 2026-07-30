af3406398e22ed1b3089a416900fd30a142ec873
Muhammad-Raheel04 <raheel31102004@gmail.com>
2026-07-30 14:21:37 +0500

    feat: integrate Swagger UI into Express server

--- server.js
+++ server.js
@@ -3,12 +3,16 @@ import express from 'express';
 import cors from 'cors';
 import './config/db.js';
 import authRoutes from './routes/authRoutes.js';
+import swaggerUi from 'swagger-ui-express';
+import swaggerSpec from './swagger.js';
+
 
 const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(cors());
+
 
 app.use((err, req, res, next) => {
     err.statusCode = err.statusCode || 500;
@@ -20,7 +24,8 @@ app.use((err, req, res, next) => {
     })
 })
 
-app.use('/api/v1/auth',authRoutes)
+app.use('/api/v1/auth', authRoutes)
+app.use('/api/v1/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.listen(PORT, () => {
     console.log(`Visit http://localhost:${PORT}`)
 })
